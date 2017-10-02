49df2c5999f681c937284c6157eceea2fe9cd519
Ines Dorian Guett <gue@danke.fish>
2017-10-03 00:01:56 +0200

    bank_room function checks now for type first

--- task_3_function.py
+++ task_3_function.py
@@ -15,11 +15,14 @@ def bank_room():
 
     choice = input("This room is full of money. How many bank note bundles do you take? ")
 
-    if int(choice) > 0 and int(choice) < 50: 
-        print("Nice, you're not greedy, you win!")
-        exit(0)
-    elif int(choice) > 50:
-        dead("You greedy bastard!")
+    if choice.isdigit():
+
+        if int(choice) > 0 and int(choice) < 50: 
+            print("Nice, you're not greedy, you win!")
+            exit(0)
+        elif int(choice) > 50:
+            dead("You greedy bastard!")
+
     else:
         dead("Man, learn to type a number.")
 
